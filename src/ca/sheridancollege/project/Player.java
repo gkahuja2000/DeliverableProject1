6488678053186a73a89429239222c8db3cd27196
gkahuja2000 <50668902+gkahuja2000@users.noreply.github.com>
2019-06-12 18:08:22 -0400

    Update Player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -4,40 +4,49 @@
  * Add your name as a modifier and the date!
  */
 package ca.sheridancollege.project;
-import java.util.*;
+
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  * @author warriors 11-06-2019
  */
-public class Player 
+public abstract class Player 
 {
-
-    private ArrayList<Card> deck;
-
-    Player() {
-        deck = new ArrayList<Card>();
-        for (int i = 0; i < 4; i++) {
-            for (int j = 1; j <= 13; j++) {
-                deck.add(new Card(i, j));
-            }
-        }
+    private String playerID; //the unique ID for this player
+    
+    /**
+     * A constructor that allows you to set the player's unique ID
+     * @param name the unique ID to assign to this player.
+     */
+    public Player(String name)
+    {
+        playerID= name;// value of name is assigned to playerID
+    }
+    
+    /**
+     * @return the playerID
+     */
+    public String getPlayerID() 
+    {
+        return playerID;//this method will retun player's name We used this method because our playerID variable was private 
+        // in order to return its value we need to call get method.
     }
 
-    public void shuffle() {
-        Random random = new Random();
-        Card temp;
-        for (int i = 0; i < 200; i++) {
-            int index1 = random.nextInt(deck.size() - 1);
-            int index2 = random.nextInt(deck.size() - 1);
-            temp = deck.get(index2);
-            deck.set(index2, deck.get(index1));
-            deck.set(index1, temp);
-        }
+    /**
+     * Ensure that the playerID is unique
+     * @param givenID the playerID to set
+     */
+    public void setPlayerID(String givenID) 
+    {
+        playerID = givenID;//this method will set player's name We used this method because our playerID variable was private 
+        // in order to reset its value we need to call set method.
     }
-
-    public Card drawCard() {
-        return deck.remove(0);
-    }
+    
+    /**
+     * The method to be instantiated when you subclass the Player class
+     * with your specific type of Player and filled in with logic to play your game.
+     */
+    public abstract void play();
+    // play method is called
+    //void is used because it will not return anything.
+   // abstract is used when class or method is not to be intilized.
 }
-
-
