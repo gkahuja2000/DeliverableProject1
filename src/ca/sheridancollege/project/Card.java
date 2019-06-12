0ab909546369bfb1ffd0d8efe46c4cd3e0834429
gkahuja2000 <50668902+gkahuja2000@users.noreply.github.com>
2019-06-12 18:09:28 -0400

    Update Card.java

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -11,46 +11,16 @@ package ca.sheridancollege.project;
  * should remember to add themselves as a modifier.
  * @author warriors 11-06-2019
  */
-public  class Card 
+public abstract class Card 
 {
-        private int position;
-        private int suit;
-        private int value;
-        private static String[] positions = {"Joker","Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"};
-        private static String[] suits = {"Diamonds","Clubs","Hearts","Spades"};
-        
-        Card(int suit, int values){
-        this.position = values;
-        this.suit = suit;
-        }
-        public String toString(){
-        return positions[position]+" of "+suits[suit];
-        }
-
-    public int getPosition() {
-        return position;
-    }
-
-    public int getSuit() {
-        return suit;
-    }
-
-    public int getValue() {
-        if(position>10){
-            value=10;
-        }
-        else if(position==1){
-          value = 11;
-        }
-        else{
-          value = position;
-        }
-        return value;
-    }
-
-    public void setValue(int value) {
-        this.value = value;
-    }
-        
+    //default modifier for child classes
+    
+    /**
+     * Students should implement this method for their specific children classes 
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    
+    @Override
+    public abstract String toString();//toString method is created in order to return string value for message for the program.
+    
 }
-
